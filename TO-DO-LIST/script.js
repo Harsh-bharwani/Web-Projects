af956fa6873473db17d62f5e2d1687b079128970
Harsh Bharwani <harshbharwani1234@gmail.com>
2025-01-20 09:34:18 +0530

    adding to do list

---
+++ TO-DO-LIST/script.js
@@ -0,0 +1,82 @@
+let addTaskbtn=document.querySelector("#add-task-btn");
+let main=document.querySelector("main");
+let taskContainer=document.querySelector(".task-container");
+let taskList=document.querySelector(".task-list")
+let modeBtn=document.querySelector("#mode-btn")
+// let resetBtn=
+addTaskbtn.addEventListener("click", ()=>{
+    let li=document.createElement("li");
+    let delBtn=document.querySelector("#del-btn");
+    taskList.append(li);
+    li.classList.add("bg-secondary")
+    li.innerHTML=`<div class="d-flex justify-content-between">
+                    <div class="m-2 p-1">    
+                        <input id="task-checkbox" type="checkbox">
+                         <input id="task-input" required type="text" value="task">
+                    </div>
+                    <button id="del-btn" class="btn btn-danger m-2 me-4">Delete</button>
+                </div>
+                <div class="features d-flex justify-content-around m-2">
+                    <span>
+                        <select class="form-select me-1" name="priority" id="priority-select">
+                            <option value="" selected disabled>priority</option>
+                            <option value="high">High</option>
+                            <option value="medium">Medium</option>
+                            <option value="low">Low</option>
+                        </select>
+                    </span>
+                    <span>
+                        <select class="form-select me-1" name="deadline" id="deadline-select">
+                            <option value="" selected disabled>deadline</option>
+                            <option value="today">Today</option>
+                            <option value="tomorrow">Tomorrow</option>
+                            <option value="thisWeek">This Week</option>
+                        </select>
+                    </span>
+                    <span>
+                        <select class="form-select me-1" name="category" id="category-select">
+                            <option value="" selected disabled>category</option>
+                            <option value="personal">personal</option>
+                            <option value="work">work</option>
+                        </select>
+                    </span>
+                    <span>
+                        <select class="form-select me-1" name="repeatation" id="repeatation-select">
+                            <option value="" selected disabled>repetition</option>
+                            <option value="daily">daily</option>
+                            <option value="weekly">weekly</option>
+                            <option value="monthly">monthly</option>
+                        </select>
+                    </span>
+                </div>`;
+    li.querySelector('#del-btn').addEventListener('click', () => {
+        li.remove();
+    });
+    li.querySelector("#task-checkbox").addEventListener("change", ()=>{
+        if(!li.querySelector("#task-checkbox").checked){
+            li.querySelector("#task-input").classList.remove("strikethrough");
+            oldProgVal=document.querySelector("progress").getAttribute("value");
+            document.querySelector("progress").setAttribute("value",Number(oldProgVal)-1);
+        }
+        else{
+            li.querySelector("#task-input").classList.add("strikethrough");
+            oldProgVal=document.querySelector("progress").getAttribute("value");
+            document.querySelector("progress").setAttribute("value",Number(oldProgVal)+1)
+        }
+    })
+    oldProgMax=document.querySelector("progress").getAttribute("max");
+    document.querySelector("progress").setAttribute("max",Number(oldProgMax)+1);
+    console.log(Number(oldProgMax)+1);
+    
+});
+
+modeBtn.addEventListener("click", ()=>{
+    if(document.body.classList.contains("bg-light")){
+        document.body.classList.add("bg-dark", "text-light");
+        document.body.classList.remove("bg-light", "text-dark");
+    }
+    else{
+        document.body.classList.add("bg-light", "text-dark");
+        document.body.classList.remove("bg-dark", "text-light");
+    }
+});
